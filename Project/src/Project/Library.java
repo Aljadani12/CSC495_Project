f42f582c1a7e9e42865b03b3fbc29d8f44fb155e
Aljadani12 <57232748+Aljadani12@users.noreply.github.com>
2022-07-26 09:02:51 -0700

    n

--- Project/src/Project/Library.java
+++ Project/src/Project/Library.java
@@ -1,12 +1,22 @@
-package Project;
-
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Library {
-
-	private JFrame frame;
+public class Library extends JFrame {
+	static Library frame;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -15,8 +25,8 @@ public class Library {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Library window = new Library();
-					window.frame.setVisible(true);
+					frame= new Library();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -25,19 +35,64 @@ public class Library {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Library() {
-		initialize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblLibraryManagement = new JLabel("Library Management");
+		lblLibraryManagement.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblLibraryManagement.setForeground(Color.GRAY);
+		
+		JButton btnAdminLogin = new JButton("Admin Login");
+		btnAdminLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			AdminLogin.main(new String[]{});
+			frame.dispose();
+			}
+		});
+		btnAdminLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JButton btnLibrarianLogin = new JButton("Librarian Login");
+		btnLibrarianLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				LibrarianLogin.main(new String[]{});
+			}
+		});
+		
+		btnLibrarianLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(64)
+							.addComponent(lblLibraryManagement))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(140)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(btnLibrarianLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnAdminLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE))))
+					.addContainerGap(95, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblLibraryManagement)
+					.addGap(32)
+					.addComponent(btnAdminLogin, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnLibrarianLogin, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(70, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+		
+		
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 }
